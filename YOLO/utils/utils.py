760b7fdafe5060c429bbc306ff5e7c982814fbe0
yeqinghuang516 <47376258+yeqinghuang516@users.noreply.github.com>
2019-06-08 18:54:07 -0700

    Update utils.py

--- YOLO/utils/utils.py
+++ YOLO/utils/utils.py
@@ -330,6 +330,7 @@ def build_targets(pred_boxes, pred_cls, target, anchors, ignore_thres):
     
 def Detect(image, model):
   classes = ['aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus', 'car', 'cat', 'chair', 'cow', 'diningtable', 'dog', 'horse', 'motorbike', 'person', 'pottedplant', 'sheep', 'sofa', 'train', 'tvmonitor']
+  img_size = 224
   transform = tv.transforms.Compose([tv.transforms.Resize((img_size, img_size)),tv.transforms.ToTensor(),tv.transforms.Normalize(mean=[0.5, 0.5, 0.5],std=[0.5, 0.5, 0.5])])
   model.eval()
 
